4cd2f6862e8a32c1ffd881a736f4081d9d20e83b
Jess Ovied <jess.oviedo49@gmail.com>
2024-08-28 14:18:15 +0200

    init

---
+++ module/documents/actor.mjs
@@ -0,0 +1,143 @@
+/**
+ * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
+ * @extends {Actor}
+ */
+export class ShinzoActor extends Actor {
+  /** @override */
+  prepareData() {
+    // Prepare data for the actor. Calling the super version of this executes
+    // the following, in order: data reset (to clear active effects),
+    // prepareBaseData(), prepareEmbeddedDocuments() (including active effects),
+    // prepareDerivedData().
+    super.prepareData();
+  }
+
+  /** @override */
+  prepareBaseData() {
+    // Data modifications in this step occur before processing embedded
+    // documents or derived data.
+  }
+
+  /**
+   * @override
+   * Augment the actor source data with additional dynamic data. Typically,
+   * you'll want to handle most of your calculated/derived data in this step.
+   * Data calculated in this step should generally not exist in template.json
+   * (such as ability modifiers rather than ability scores) and should be
+   * available both inside and outside of character sheets (such as if an actor
+   * is queried and has a roll executed directly from it).
+   */
+  prepareDerivedData() {
+    const actorData = this;
+    const systemData = actorData.system;
+    const flags = actorData.flags.shinzo || {};
+
+    // Make separate methods for each Actor type (character, npc, etc.) to keep
+    // things organized.
+    this._prepareCharacterData(actorData);
+    this._prepareNpcData(actorData);
+  }
+
+  /**
+   * Prepare Character type specific data
+   */
+  _prepareCharacterData(actorData) {
+    if (actorData.type !== 'character') return;
+
+    // Make modifications to data here. For example:
+    const systemData = actorData.system;
+
+      let gds_stats = systemData.gds_stats;
+      let maitrise = systemData.maitrises;
+      let ss_stat1 = systemData.ss_stats1;
+      let ss_stat2 = systemData.ss_stats2;
+      
+      maitrise.cor.value = Math.floor((2*gds_stats.con.value + 2*gds_stats.agi.value + gds_stats.sav.value) / 5 + maitrise.cor.b_m);
+      maitrise.esp.value = Math.floor((2*gds_stats.mag.value + 2*gds_stats.men.value + gds_stats.sav.value) / 5 + maitrise.esp.b_m);
+      maitrise.soc.value = Math.floor((gds_stats.sav.value + 4*gds_stats.cha.value) / 5 + maitrise.soc.b_m);
+
+      ss_stat1.pug.value = Math.floor((2*gds_stats.con.value + maitrise.cor.value) / 3 + ss_stat1.pug.b_m);
+      ss_stat1.cac.value = Math.floor((gds_stats.con.value + 2*maitrise.cor.value) / 3 + ss_stat1.cac.b_m);
+      ss_stat1.pre.value = Math.floor((maitrise.esp.value + 3*maitrise.cor.value) / 4 + ss_stat1.pre.b_m);
+      ss_stat1.esq.value = Math.floor((maitrise.cor.value + 2*gds_stats.agi.value) / 3 + ss_stat1.esq.b_m);
+      ss_stat1.par.value = Math.floor(maitrise.cor.value + ss_stat1.par.b_m);
+      ss_stat1.dis.value = Math.floor((maitrise.soc.value + 2*gds_stats.agi.value) / 3 + ss_stat1.dis.b_m);
+      ss_stat1.obs.value = Math.floor((maitrise.soc.value + 3*gds_stats.men.value) / 4 + ss_stat1.obs.b_m);
+      ss_stat1.per.value = Math.floor((maitrise.esp.value + 3*gds_stats.mag.value) / 4 + ss_stat1.per.b_m);
+      ss_stat1.psy.value = Math.floor((gds_stats.mag.value + gds_stats.men.value + 2*maitrise.esp.value) / 4 + ss_stat1.psy.b_m);
+      ss_stat1.int.value = Math.floor((gds_stats.con.value + 2*maitrise.soc.value) / 3 + ss_stat1.int.b_m);
+      ss_stat2.com.value = Math.floor(maitrise.soc.value + ss_stat2.com.b_m);
+      ss_stat2.aut.value = Math.floor((maitrise.soc.value + gds_stats.cha.value) / 2 + ss_stat2.aut.b_m);
+      ss_stat2.med.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.med.b_m);
+      ss_stat2.pil.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.pil.b_m);
+      ss_stat2.art.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.art.b_m);
+      ss_stat2.vit.value = Math.floor((gds_stats.agi.value + maitrise.cor.value) / 2 + ss_stat2.vit.b_m);
+      ss_stat2.ins.value = Math.floor((gds_stats.men.value + maitrise.esp.value) / 2 + ss_stat2.ins.b_m);
+      ss_stat2.s_f.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.s_f.b_m);
+      ss_stat2.reg.value = Math.floor((gds_stats.con.value + maitrise.esp.value) / 2 + ss_stat2.reg.b_m);
+      ss_stat2.rec.value = Math.floor((gds_stats.mag.value + maitrise.esp.value) / 2 + ss_stat2.rec.b_m);
+
+      // Loop through ability scores, and add their modifiers to our sheet output.
+    /*for (let [key, maitrise] of Object.entries(systemData.maitrises)) {
+      // Calculate the modifier using d20 rules.
+      console.log(maitrise)
+      maitrise.mod = Math.floor(maitrise.value + maitrise.b_m);
+      console.log(maitrise.mod)
+    }*/
+      // Calculate the modifier using d20 rules.
+  }
+
+  /**
+   * Prepare NPC type specific data.
+   */
+  _prepareNpcData(actorData) {
+    if (actorData.type !== 'npc') return;
+
+    // Make modifications to data here. For example:
+    const systemData = actorData.system;
+    systemData.xp = systemData.cr * systemData.cr * 100;
+  }
+
+  /**
+   * Override getRollData() that's supplied to rolls.
+   */
+  getRollData() {
+    // Starts off by populating the roll data with a shallow copy of `this.system`
+    const data = { ...this.system };
+
+    // Prepare character roll data.
+    this._getCharacterRollData(data);
+    this._getNpcRollData(data);
+
+    return data;
+  }
+
+  /**
+   * Prepare character roll data.
+   */
+  _getCharacterRollData(data) {
+    if (this.type !== 'character') return;
+
+    // Copy the ability scores to the top level, so that rolls can use
+    // formulas like `@str.mod + 4`.
+    if (data.gds_stats) {
+      for (let [k, v] of Object.entries(data.gds_stats)) {
+        data[k] = foundry.utils.deepClone(v);
+      }
+    }
+
+    // Add level for easier access, or fall back to 0.
+    if (data.valuation.rating) {
+      data.lvl = data.valuation.rating.value ?? 0;
+    }
+  }
+
+  /**
+   * Prepare NPC roll data.
+   */
+  _getNpcRollData(data) {
+    if (this.type !== 'npc') return;
+
+    // Process additional NPC data here.
+  }
+}
