b597d2a94102f39b286622bcb319ac7be93b0250
Jess Ovied <jess.oviedo49@gmail.com>
2024-09-02 16:13:01 +0200

    ajout des contextuels de traits + grandes stats brut + ajout mod dans templates ss_stats

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -53,34 +53,41 @@ export class ShinzoActor extends Actor {
       let ss_stats = systemData.ss_stats;
       let ss_stat1 = ss_stats.ss_stats1;
       let ss_stat2 = ss_stats.ss_stats2;
+
+      gds_stats.con.brut = Math.floor(gds_stats.con.value + gds_stats.con.b_m);
+      gds_stats.agi.brut = Math.floor(gds_stats.agi.value + gds_stats.agi.b_m);
+      gds_stats.cha.brut = Math.floor(gds_stats.cha.value + gds_stats.cha.b_m);
+      gds_stats.sav.brut = Math.floor(gds_stats.sav.value + gds_stats.sav.b_m);
+      gds_stats.mag.brut = Math.floor(gds_stats.mag.value + gds_stats.mag.b_m);
+      gds_stats.men.brut = Math.floor(gds_stats.men.value + gds_stats.men.b_m);
       
       maitrise.cor.value = Math.floor((2*gds_stats.con.value + 2*gds_stats.agi.value + gds_stats.sav.value) / 5 + maitrise.cor.b_m);
       maitrise.esp.value = Math.floor((2*gds_stats.mag.value + 2*gds_stats.men.value + gds_stats.sav.value) / 5 + maitrise.esp.b_m);
       maitrise.soc.value = Math.floor((gds_stats.sav.value + 4*gds_stats.cha.value) / 5 + maitrise.soc.b_m);
 
-      ss_stat1.pug.value = Math.floor((2*gds_stats.con.value + maitrise.cor.value) / 3 + ss_stat1.pug.b_m);
-      ss_stat1.cac.value = Math.floor((gds_stats.con.value + 2*maitrise.cor.value) / 3 + ss_stat1.cac.b_m);
-      ss_stat1.pre.value = Math.floor((maitrise.esp.value + 3*maitrise.cor.value) / 4 + ss_stat1.pre.b_m);
-      ss_stat1.esq.value = Math.floor((maitrise.cor.value + 2*gds_stats.agi.value) / 3 + ss_stat1.esq.b_m);
-      ss_stat1.par.value = Math.floor(maitrise.cor.value + ss_stat1.par.b_m);
-      ss_stat1.dis.value = Math.floor((maitrise.soc.value + 2*gds_stats.agi.value) / 3 + ss_stat1.dis.b_m);
-      ss_stat1.obs.value = Math.floor((maitrise.soc.value + 3*gds_stats.men.value) / 4 + ss_stat1.obs.b_m);
-      ss_stat1.per.value = Math.floor((maitrise.esp.value + 3*gds_stats.mag.value) / 4 + ss_stat1.per.b_m);
-      ss_stat1.psy.value = Math.floor((gds_stats.mag.value + gds_stats.men.value + 2*maitrise.esp.value) / 4 + ss_stat1.psy.b_m);
-      ss_stat1.int.value = Math.floor((gds_stats.con.value + 2*maitrise.soc.value) / 3 + ss_stat1.int.b_m);
-      ss_stat2.com.value = Math.floor(maitrise.soc.value + ss_stat2.com.b_m);
-      ss_stat2.aut.value = Math.floor((maitrise.soc.value + gds_stats.cha.value) / 2 + ss_stat2.aut.b_m);
-      ss_stat2.med.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.med.b_m);
-      ss_stat2.pil.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.pil.b_m);
-      ss_stat2.art.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.art.b_m);
-      ss_stat2.vit.value = Math.floor((gds_stats.agi.value + maitrise.cor.value) / 2 + ss_stat2.vit.b_m);
-      ss_stat2.ins.value = Math.floor((gds_stats.men.value + maitrise.esp.value) / 2 + ss_stat2.ins.b_m);
-      ss_stat2.s_f.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.s_f.b_m);
-      ss_stat2.reg.value = Math.floor((gds_stats.con.value + maitrise.esp.value) / 2 + ss_stat2.reg.b_m);
-      ss_stat2.rec.value = Math.floor((gds_stats.mag.value + maitrise.esp.value) / 2 + ss_stat2.rec.b_m);
+      ss_stat1.pug.value = Math.floor((2*gds_stats.con.value + maitrise.cor.value) / 3 + ss_stat1.pug.mod + ss_stat1.pug.b_m);
+      ss_stat1.cac.value = Math.floor((gds_stats.con.value + 2*maitrise.cor.value) / 3 + ss_stat1.cac.mod + ss_stat1.cac.b_m);
+      ss_stat1.pre.value = Math.floor((maitrise.esp.value + 3*maitrise.cor.value) / 4 + ss_stat1.pre.mod + ss_stat1.pre.b_m);
+      ss_stat1.esq.value = Math.floor((maitrise.cor.value + 2*gds_stats.agi.value) / 3 + ss_stat1.esq.mod + ss_stat1.esq.b_m);
+      ss_stat1.par.value = Math.floor(maitrise.cor.value + ss_stat1.par.mod + ss_stat1.par.b_m);
+      ss_stat1.dis.value = Math.floor((maitrise.soc.value + 2*gds_stats.agi.value) / 3 + ss_stat1.dis.mod + ss_stat1.dis.b_m);
+      ss_stat1.obs.value = Math.floor((maitrise.soc.value + 3*gds_stats.men.value) / 4 + ss_stat1.obs.mod + ss_stat1.obs.b_m);
+      ss_stat1.per.value = Math.floor((maitrise.esp.value + 3*gds_stats.mag.value) / 4 + ss_stat1.per.mod + ss_stat1.per.b_m);
+      ss_stat1.psy.value = Math.floor((gds_stats.mag.value + gds_stats.men.value + 2*maitrise.esp.value) / 4 + ss_stat1.psy.mod + ss_stat1.psy.b_m);
+      ss_stat1.int.value = Math.floor((gds_stats.con.value + 2*maitrise.soc.value) / 3 + ss_stat1.int.mod + ss_stat1.int.b_m);
+      ss_stat2.com.value = Math.floor(maitrise.soc.value + ss_stat2.com.mod + ss_stat2.com.b_m);
+      ss_stat2.aut.value = Math.floor((maitrise.soc.value + gds_stats.cha.value) / 2 + ss_stat2.aut.mod + ss_stat2.aut.b_m);
+      ss_stat2.med.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.med.mod + ss_stat2.med.b_m);
+      ss_stat2.pil.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.pil.mod + ss_stat2.pil.b_m);
+      ss_stat2.art.value = Math.floor((maitrise.soc.value + maitrise.esp.value) / 2 + ss_stat2.art.mod + ss_stat2.art.b_m);
+      ss_stat2.vit.value = Math.floor((gds_stats.agi.value + maitrise.cor.value) / 2 + ss_stat2.vit.mod + ss_stat2.vit.b_m);
+      ss_stat2.ins.value = Math.floor((gds_stats.men.value + maitrise.esp.value) / 2 + ss_stat2.ins.mod + ss_stat2.ins.b_m);
+      ss_stat2.s_f.value = Math.floor((gds_stats.sav.value + maitrise.esp.value) / 2 + ss_stat2.s_f.mod + ss_stat2.s_f.b_m);
+      ss_stat2.reg.value = Math.floor((gds_stats.con.value + maitrise.esp.value) / 2 + ss_stat2.reg.mod + ss_stat2.reg.b_m);
+      ss_stat2.rec.value = Math.floor((gds_stats.mag.value + maitrise.esp.value) / 2 + ss_stat2.rec.mod + ss_stat2.rec.b_m);
 
       valuation.value = Math.floor(ss_stat1.pug.value + ss_stat1.cac.value + ss_stat1.pre.value + ss_stat1.esq.value + ss_stat1.par.value + ss_stat1.dis.value + ss_stat1.obs.value + ss_stat1.per.value + ss_stat1.psy.value + ss_stat1.int.value + ss_stat2.com.value + ss_stat2.aut.value + ss_stat2.med.value + ss_stat2.pil.value + ss_stat2.art.value + ss_stat2.vit.value + ss_stat2.ins.value + ss_stat2.s_f.value + ss_stat2.reg.value + ss_stat2.rec.value - 900);
-      ss_stats.totalb_m.value = Math.floor(ss_stat1.pug.b_m + ss_stat1.cac.b_m + ss_stat1.pre.b_m + ss_stat1.esq.b_m + ss_stat1.par.b_m + ss_stat1.dis.b_m + ss_stat1.obs.b_m + ss_stat1.per.b_m + ss_stat1.psy.b_m + ss_stat1.int.b_m + ss_stat2.com.b_m + ss_stat2.aut.b_m + ss_stat2.med.b_m + ss_stat2.pil.b_m + ss_stat2.art.b_m + ss_stat2.vit.b_m + ss_stat2.ins.b_m + ss_stat2.s_f.b_m + ss_stat2.reg.b_m + ss_stat2.rec.b_m);
+      ss_stats.totalmod.value = Math.floor(ss_stat1.pug.mod + ss_stat1.cac.mod + ss_stat1.pre.mod + ss_stat1.esq.mod + ss_stat1.par.mod + ss_stat1.dis.mod + ss_stat1.obs.mod + ss_stat1.per.mod + ss_stat1.psy.mod + ss_stat1.int.mod + ss_stat2.com.mod + ss_stat2.aut.mod + ss_stat2.med.mod + ss_stat2.pil.mod + ss_stat2.art.mod + ss_stat2.vit.mod + ss_stat2.ins.mod + ss_stat2.s_f.mod + ss_stat2.reg.mod + ss_stat2.rec.mod);
       // Loop through ability scores, and add their modifiers to our sheet output.
     /*for (let [key, maitrise] of Object.entries(systemData.maitrises)) {
       // Calculate the modifier using d20 rules.
